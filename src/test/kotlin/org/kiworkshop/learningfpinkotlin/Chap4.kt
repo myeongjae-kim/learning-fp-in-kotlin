118d69fdadeefd393a799e67e47a787005dcf4bc
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2021-11-13 16:57:04 +0900

    [#9] Solve 4-2

--- src/test/kotlin/org/kiworkshop/learningfpinkotlin/Chap4.kt
+++ src/test/kotlin/org/kiworkshop/learningfpinkotlin/Chap4.kt
@@ -24,4 +24,21 @@ class Chap4 : StringSpec({
         isEvenOrOdd(100) shouldBe "100 is even"
         isEvenOrOdd(99) shouldBe "99 is odd"
     }
+
+    "Example 4-2" {
+        fun <P1, P2, P3, R> ((P1, P2, P3) -> R).partial1(p1: P1): (P2, P3) -> R =
+            { p2, p3 -> this(p1, p2, p3) }
+
+        fun <P1, P2, P3, R> ((P1, P2, P3) -> R).partial2(p2: P2): (P1, P3) -> R =
+            { p1, p3 -> this(p1, p2, p3) }
+
+        fun <P1, P2, P3, R> ((P1, P2, P3) -> R).partial3(p3: P3): (P1, P2) -> R =
+            { p1, p2 -> this(p1, p2, p3) }
+
+        val f: (String, String, String) -> String = { p1, p2, p3 -> "$p1, $p2, $p3" }
+
+        f.partial1("alpha")("beta", "gamma") shouldBe "alpha, beta, gamma"
+        f.partial2("alpha")("beta", "gamma") shouldBe "beta, alpha, gamma"
+        f.partial3("alpha")("beta", "gamma") shouldBe "beta, gamma, alpha"
+    }
 })
