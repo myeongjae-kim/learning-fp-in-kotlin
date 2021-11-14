c133bf6bead1ff3d519b0ee56083da0ce17c928a
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2021-11-14 19:58:41 +0900

    [#14] Solve 5-3

--- src/main/kotlin/org/kiworkshop/learningfpinkotlin/FunList.kt
+++ src/main/kotlin/org/kiworkshop/learningfpinkotlin/FunList.kt
@@ -1,6 +1,34 @@
 package org.kiworkshop.learningfpinkotlin
 
+import org.kiworkshop.learningfpinkotlin.FunList.Cons
+import org.kiworkshop.learningfpinkotlin.FunList.Nil
+
 sealed class FunList<out T> {
     object Nil : FunList<Nothing>()
     data class Cons<out T>(val head: T, val tail: FunList<T>) : FunList<T>()
+}
+
+fun <T> FunList<T>.addHead(head: T): FunList<T> = Cons(head, this)
+
+tailrec fun <T> FunList<T>.appendTail(value: T, acc: FunList<T> = Nil): FunList<T> = when (this) {
+    Nil -> Cons(value, acc).reverse()
+    is Cons -> tail.appendTail(value, acc.addHead(head))
+    // 매번 새로운 list를 생성하지 않으면 immutable하지 않게 된다.
+    // 마지막 element 뒤에 Cons를 하나 더 붙이면 기존의 list에도 변화가 있는 것이니,
+    // immutable하기 위해서는 addHead와 appendTail중 하나는 O(n)이 되어야 한다.
+}
+
+tailrec fun <T> FunList<T>.reverse(acc: FunList<T> = Nil): FunList<T> = when (this) {
+    Nil -> acc
+    is Cons -> tail.reverse(acc.addHead(head))
+}
+
+fun <T> FunList<T>.getTail(): FunList<T> = when (this) {
+    Nil -> throw NoSuchElementException()
+    is Cons -> tail
+}
+
+fun <T> FunList<T>.getHead(): T = when (this) {
+    Nil -> throw NoSuchElementException()
+    is Cons -> head
 }
--- src/test/kotlin/org/kiworkshop/learningfpinkotlin/Chap5.kt
+++ src/test/kotlin/org/kiworkshop/learningfpinkotlin/Chap5.kt
@@ -2,6 +2,7 @@ package org.kiworkshop.learningfpinkotlin
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.shouldBe
 import org.kiworkshop.learningfpinkotlin.FunList.Cons
 import org.kiworkshop.learningfpinkotlin.FunList.Nil
 
@@ -29,5 +30,13 @@ class Chap5 : StringSpec({
 
         // then
         doubleList.toList().shouldContainExactly(1.0, 2.0, 3.0, 4.0, 5.0)
+    }
+
+    "Example 5-3" {
+        Cons(1, Nil).addHead(2).toList().shouldContainExactly(2, 1)
+        Cons(1, Nil).appendTail(2).toList().shouldContainExactly(1, 2)
+        Cons(1, Nil).appendTail(2).reverse().toList().shouldContainExactly(2, 1)
+        Cons(1, Nil).addHead(2).addHead(3).getTail().toList().shouldContainExactly(2, 1)
+        Cons(1, Nil).addHead(2).addHead(3).getHead() shouldBe 3
     }
 })
