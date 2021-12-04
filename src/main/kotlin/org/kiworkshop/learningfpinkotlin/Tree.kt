468c2db56d5ab2d5296cef5a1a6188d0908adf33
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2021-12-04 21:14:30 +0900

    [#15] Solve 8-5

---
+++ src/main/kotlin/org/kiworkshop/learningfpinkotlin/Tree.kt
@@ -0,0 +1,20 @@
+package org.kiworkshop.learningfpinkotlin
+
+sealed class Tree<out A> : Functor<A> {
+    abstract override fun <B> fmap(f: (A) -> B): Tree<B>
+
+    companion object
+}
+
+data class Node<out A>(val value: A, val forest: List<Node<A>> = emptyList()) : Tree<A>() {
+    override fun toString(): String = "$value $forest"
+
+    override fun <B> fmap(f: (A) -> B): Node<B> = Node(f(value), forest.map { it.fmap(f) })
+}
+
+fun <A> Tree.Companion.pure(value: A) = Node(value)
+
+infix fun <A, B> Node<(A) -> B>.apply(node: Node<A>): Node<B> = Node(
+    value(node.value),
+    node.forest.map { it.fmap(value) } + forest.map { it.apply(node) }
+)
--- src/test/kotlin/org/kiworkshop/learningfpinkotlin/Chap8.kt
+++ src/test/kotlin/org/kiworkshop/learningfpinkotlin/Chap8.kt
@@ -22,4 +22,31 @@ class Chap8 : StringSpec({
 
         FunList.pure(10) shouldBe funListOf(10)
     }
+
+    "Example 4" {
+        val tree = Node(1, listOf(Node(2), Node(3)))
+        tree.fmap { it * 2 }.toString() shouldBe "2 [4 [], 6 []]"
+
+        (Tree.pure { x: Int -> x * 2 } apply tree).toString() shouldBe "2 [4 [], 6 []]"
+
+        (
+            Tree.pure({ x: Int, y: Int -> x * y }.curried())
+                apply Node(1, listOf(Node(2), Node(3)))
+                apply Node(4, listOf(Node(5), Node(6)))
+            ).toString() shouldBe "4 [5 [], 6 [], 8 [10 [], 12 []], 12 [15 [], 18 []]]"
+
+        (
+            Tree.pure({ x: Int, y: Int -> x * y }.curried())
+                apply Node(4, listOf(Node(5), Node(6)))
+                apply Node(1, listOf(Node(2), Node(3)))
+            ).toString() shouldBe "4 [8 [], 12 [], 5 [10 [], 15 []], 6 [12 [], 18 []]]"
+    }
+
+    "Example 5" {
+        (
+            Tree.pure({ x: Int, y: Int -> x * y }.curried())
+                apply Node(1, listOf(Node(2, listOf(Node(3))), Node(4)))
+                apply Node(5, listOf(Node(6), Node(7, listOf(Node(8), Node(9)))))
+            ).toString() shouldBe "5 [6 [], 7 [8 [], 9 []], 10 [12 [], 14 [16 [], 18 []], 15 [18 [], 21 [24 [], 27 []]]], 20 [24 [], 28 [32 [], 36 []]]]"
+    }
 })
