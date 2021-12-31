f89a2f5b8dd2018849643afb09fb1d420080f7ae
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2021-12-31 19:38:18 +0900

    [#18] Solve 10-3

--- src/test/kotlin/org/kiworkshop/learningfpinkotlin/Chap10.kt
+++ src/test/kotlin/org/kiworkshop/learningfpinkotlin/Chap10.kt
@@ -2,17 +2,34 @@ package org.kiworkshop.learningfpinkotlin
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import org.kiworkshop.learningfpinkotlin.Maybe.Just
 
 class Chap10 : StringSpec({
 
     "Example 10-1 ~ 10-2" {
         funListOf(1, 2, 3).run {
             flatMap { funListOf(it * 2) } shouldBe funListOf(2, 4, 6)
-            flatMap { Maybe.Just(it * 2) } shouldBe FunList.Nil
+            flatMap { Just(it * 2) } shouldBe FunList.Nil
 
             leadTo(funListOf(4, 5, 6)) shouldBe funListOf(4, 5, 6, 4, 5, 6, 4, 5, 6) // ???????????
             pure(4) shouldBe funListOf(4)
             // apply는 테스트 안해도 되겠는데?
         }
+    }
+
+    "Example 10-3" {
+        class D4(val value: Maybe<String>)
+        class C4(val d: D4)
+        class B4(val c: Maybe<C4>)
+        class A4(val b: Maybe<B4>)
+
+        fun getValueOfD4(a: A4): Maybe<String> = a.b
+            .flatMap { it.c }
+            .fmap { it.d }
+            .flatMap { it.value }
+
+        val value = "what..?"
+
+        getValueOfD4(A4(Just(B4(Just(C4(D4(Just(value)))))))).toString() shouldBe "Just($value)"
     }
 })
